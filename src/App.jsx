dc6747489139c01036188f7cd5071777d96aa46a
Victor Manuel Morillo Howley <victormh@IBSYSTEMS.COM>
2023-12-04 16:19:45 -0400

    background image

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">qr</label>
     <div className="relative w-96 border-8 border-[#3662E3]">
         <input  type="text" onChange={(e) => setFormData({...formData, text: e.target.value})}  value={formData.text} id="input" className=" border-3 border-[#3662E3] w-full p-4 ps-3 text-sm text-gray-900  rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter an Url" />
-        <button type='submit' className='relative bottom-12 left-[135px] bg-[#3662E3]'>QR Code</button>
+        <button type='submit' className='relative bottom-[49px] left-[165px] bg-[#3662E3] '>QR Code</button>
     </div>
 </form>
 </div> :
